6b782114c52bb2abf078289560fe51c72d993c83
syllxvestern <syllxvestern@gmail.com>
2021-03-25 20:38:51 +0700

    Pertemuan 7

---
+++ pertemuan-ketujuh/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { reducer as formReducer } from 'redux-form';
+
+import './index.css';
+import App from './App';
+import registerServiceWorker from 'register-service-worker';
+
+const rootReducer = combineReducers({
+  form:formReducer,
+});
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// registerServiceWorker();
