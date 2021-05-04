1e4800b1e4b650d0c1b6c0f2f2700100b2a16871
syllxvestern <syllxvestern@gmail.com>
2021-05-04 21:37:28 +0700

    Pertemuan 12

---
+++ pertemuan-keduabelas/web/resources/views/welcome.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<div className="container">
+            <div className="d-flex justify-content-left">
+            <table class="table table-striped table-dark">
+  <thead>
+    <tr>
+      <th scope="col">Pemrograman Berbasis Framework</th>
+      <th scope="col"></th>
+      <th scope="col"></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Nama :</th>
+      <td>M.Rahmat Samudra Anugrah</td>
+    </tr>
+    <tr>
+      <th scope="row">Kelas :</th>
+      <td>TI-3F</td>
+    </tr>
+    <tr>
+      <th scope="row">Prodi :</th>
+      <td>D-IV Teknik Informatika</td>
+    </tr>
+    <script src="{{ asset('js/app.js') }}"></script>
+  </tbody>
+</table>
+                <div >
+                </div>
+            </div>
+        </div>
+</html>
